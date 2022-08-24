59df89318f2f39d057b7b95295ab6d291ef90665
kpomak <romka_ne@mail.ru>
2022-08-24 03:06:08 +0300

    add forms and users controller

--- flask_blog/flask_blog/__init__.py
+++ flask_blog/flask_blog/__init__.py
@@ -2,20 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_blog.config import Config
+from flask_bcrypt import Bcrypt
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+
+
     db.init_app(app)
     login_manager.init_app(app)
+    bcrypt.init_app(app)
     
     from flask_blog.main.routes import main
+    from flask_blog.users.routes import users
+
     app.register_blueprint(main)
+    app.register_blueprint(users)
 
     return app
